4b2b7982296dbcfc4742583f3bc1da789892cae9
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-05-13 11:25:55 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("Hello Mundo");
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
