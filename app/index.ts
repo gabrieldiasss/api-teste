fff2ef840cc225314878589f816626bedd28bddc
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-05-13 12:56:00 -0300

    change folder

--- app/index.ts
+++ app/index.ts
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  res.send("Salvee");
+  res.send("Oiiii");
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
